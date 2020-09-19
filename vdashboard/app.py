e3a1afa01ca9e843fa1a702e70424cfd82f78c4b
Norman Gilmore <git@teamforecast.com>
2020-09-18 18:08:39 -0700

    Lambda calls scistarter.record_participation

--- vdashboard/app.py
+++ vdashboard/app.py
@@ -1,5 +1,7 @@
+import os
 import json
 
+from scistarter import record_participation
 
 def lambda_handler(event, context):
     """Handle a Pybossa webhook
@@ -15,7 +17,11 @@ def lambda_handler(event, context):
     Returns
     ------
     """
+    # If a project's SciStarter URL is https://scistarter.org/an-example, then the slug is
+    # "an-example". This icoming path is "/project/an-example", so take the basename
+    # as the SciStarter project slug.
     path = event.get("path", "")
+    project_slug = os.path.basename(path)
     headers = event.get("multiValueHeaders", {})
     body = event.get("body", "")
     if "httpMethod" in event and event["httpMethod"] == "GET":
@@ -28,12 +34,21 @@ def lambda_handler(event, context):
             project_short_name = webhook_data["project_short_name"]
             project_id = webhook_data["project_id"]
             task_id = webhook_data["task_id"]
+            # taskrun_id is only provided by a patched version of Pybossa
+            taskrun_id = int(webhook_data.get("taskrun_id", -1))
             result_id = webhook_data["result_id"]
             event = webhook_data["event"]
-        except (json.decoder.JSONDecodeError, KeyError):
+        except (json.decoder.JSONDecodeError, KeyError, ValueError, TypeError):
             return simple_response(400, "Bad Request")
-        print("Calling external API.")
-        return simple_response(200, "200 OK", payload="Notification sent.")
+        if taskrun_id != -1:
+            print(
+                f"Notified of taskrun {taskrun_id} "
+                f"by user {user_id} in project {project_short_name}"
+            )
+            record_participation(taskrun_id, project_slug)
+            return simple_response(200, "200 OK", payload="Notification sent.")
+        else:
+            return simple_response(400, "Bad Request", payload="Webhook did not provide taskrun_id.")
     # Shouldn't reach here under normal use.
     print(path)
     print(json.dumps(headers))
